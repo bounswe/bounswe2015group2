1106b100a4a006425db049e4c56e6cd8dc9ec095
oeken <m.onureken@gmail.com>
2015-05-02 17:12:28 +0300

    print added oeken

--- module-1/src/MathematicalOperations.java
+++ module-1/src/MathematicalOperations.java
@@ -2,7 +2,8 @@
  * Created by onureken on 02/05/15.
  */
 public class MathematicalOperations {
-     public static void main(String[] args){
-        System.out.println("Acıktım rulez");
+
+     public static int dummyFunction(){
+         return 1;
      }
 }
---
+++ module-1/src/MathematicalOperationsTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class MathematicalOperationsTest {
+
+    @Test
+    public void testDummyFunction() throws Exception {
+        assertEquals("deneme" , 25, 24);
+    }
+}
