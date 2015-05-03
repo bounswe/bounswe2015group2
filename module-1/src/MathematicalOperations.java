8c4470484362c07308b0453a89203b4d42d2afd2
onur <Onur NEVAT>
2015-05-03 16:10:25 +0300

    Main function fixes

--- module-1/src/MathematicalOperations.java
+++ module-1/src/MathematicalOperations.java
@@ -18,28 +18,57 @@ public class MathematicalOperations {
     /**
      * Multiply two integer and returns the result
      * Added by Onur Neşvat
+     *
      * @param a integer1
      * @param b integer 2
      * @return result
      */
-    public static double multiply(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Which operation you would like to try: \n" +
                 "For Dummy Function, Press 1:" +
-                "For Addition Function, Press2:");
-        int functionChoice = input.nextInt();
-        if (functionChoice == 1)
-            dummyFunction();
-        else if (functionChoice == 2) {
-            Scanner input2 = new Scanner(System.in);
-            System.out.println("Enter two numbers to add: ");
-            int a = input2.nextInt();
-            int b = input2.nextInt();
-            additionFunction(a, b);
+                "For Addition Function, Press 2:" +
+                "For Divide Function, Press 3:" +
+                "For Multiply Function Press 4:" +
+                "For Exit Press 0:");
+        int functionChoice = 0;
+        while (true) {
+            System.out.print('Command: ')
+            int functionChoice = input.nextInt();
+
+            if (functionChoice == 0) {
+                System.out.println('good bye!');
+                break;
+            }
+
+            if (functionChoice == 1)
+                dummyFunction();
+            else if (functionChoice == 2) {
+                Scanner input2 = new Scanner(System.in);
+                System.out.println("Enter two numbers to add: ");
+                int a = input2.nextInt();
+                int b = input2.nextInt();
+                additionFunction(a, b);
+            } else if (functionChoice == 3) {
+                Scanner input2 = new Scanner(System.in);
+                System.out.println("Enter two numbers to divde: ");
+                int a = input2.nextInt();
+                int b = input2.nextInt();
+                System.out.println(divide(a, b));
+            } else if (functionChoice == 4) {
+                Scanner input2 = new Scanner(System.in);
+                System.out.println("Enter two numbers to multiply: ");
+                int a = input2.nextInt();
+                int b = input2.nextInt();
+                System.out.println(multiply(a, b));
+            }
         }
     }
 
