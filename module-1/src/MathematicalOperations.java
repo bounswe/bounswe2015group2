8332d6b3c6ed9f1f4662a17d893cbc126b3d733d
silaguler <silaguler90@gmail.com>
2015-05-03 14:42:02 +0300

    Test method for AdditionFunction is added to MathematicalOperationsTest.java file by Sıla Güler

--- module-1/src/MathematicalOperations.java
+++ module-1/src/MathematicalOperations.java
@@ -1,20 +1,15 @@
 /**
  * Created by onureken on 02/05/15.
  */
+import java.lang.System;
 import java.util.Scanner;
 public class MathematicalOperations {
+
     public static int dummyFunction(){
          return 1;
-     }
+     } // Added by Onur Eken
+    public static int additionFunction(int a, int b){ return a+b; } // Added by Sýla Güler
 
-    public static int additionFunction(){ // Added by Sýla Güler
-        Scanner input = new Scanner(System.in);
-        int a,b ;
-        System.out.println("Enter two numbers to add: ");
-        a = input.nextInt();
-        b = input.nextInt();
-        return a+b;
-    }
     public static void main(String[] args){
         Scanner input1 = new Scanner(System.in);
         System.out.println("Which operation you would like to try: \n" +
@@ -23,7 +18,12 @@ public class MathematicalOperations {
         int functionChoice = input.nextInt();
         if(functionChoice==1)
             dummyFunction();
-        else if (functionChoice==2)
-            additionFunction();
+        else if (functionChoice==2) {
+            Scanner input2 = new Scanner(System.in);
+            System.out.println("Enter two numbers to add: ");
+            int a = input2.nextInt();
+            int b = input2.nextInt();
+            additionFunction(a,b);
+        }
     }
 }
--- module-1/src/MathematicalOperationsTest.java
+++ module-1/src/MathematicalOperationsTest.java
@@ -1,12 +1,17 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import MathematicalOperations.java;
 public class MathematicalOperationsTest {
 
-    @Test
+    @Test // Added by Onur Eken
     public void testDummyFunction() throws Exception {
         int a = 14; // oops typo mistake
         assertEquals("Var a must equal 13",13,a);
-
+    }
+    @Test // Added by Sýla Güler
+    public void testAdditionFunction() throws Exception{
+        assertEquals("Addition of 0 and 10 must equal 10",10,additionFunction(0, 10));
+        assertEquals("Addition of 10 and 0 must equal 10",10,additionFunction(10, 0));
     }
 
 
