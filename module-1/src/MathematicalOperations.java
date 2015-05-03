99640d50e4950c4ad4153ec03edede656d7a23ca
ozan <ozanmahir@gmail.com>
2015-05-03 18:22:29 +0300

    sss

--- module-1/src/MathematicalOperations.java
+++ module-1/src/MathematicalOperations.java
@@ -52,6 +52,7 @@ public class MathematicalOperations {
         if(power == 0) return 1;
         return base * power(base, --power);
     }
+    public static int minus(int a, int b) { return a-b;}
 
     /**
      * @param args
@@ -65,6 +66,7 @@ public class MathematicalOperations {
                 "For Multiply Function Press 4:" +
                 "For Power Fuction, Press 5:" +
                 "For Remainder Function Press 6" +
+                "For Minus Function Press 7" +
                 "For Exit Press 0:");
         int functionChoice = 0;
         while (true) {
@@ -86,7 +88,7 @@ public class MathematicalOperations {
                 additionFunction(a, b);
             } else if (functionChoice == 3) {
                 Scanner input2 = new Scanner(System.in);
-                System.out.println("Enter two numbers to divde: ");
+                System.out.println("Enter two numbers to divide: ");
                 int a = input2.nextInt();
                 int b = input2.nextInt();
                 System.out.println(divide(a, b));
@@ -104,10 +106,16 @@ public class MathematicalOperations {
                 System.out.println(power(a, b));
             } else if (functionChoice == 6) {
                 Scanner input2 = new Scanner(System.in);
-                System.out.println("Enter two numbers as dividend and diviser: ");
+                System.out.println("Enter two numbers as dividend and divisor: ");
                 int a = input2.nextInt();
                 int b = input2.nextInt();
                 System.out.println(remainder(a, b));
+            } else if (functionChoice == 7) {
+                Scanner input2 = new Scanner(System.in);
+                System.out.println("Enter two numbers to subtract: ");
+                int a = input2.nextInt();
+                int b = input2.nextInt();
+                System.out.println(minus(a, b));
             }
         }
     }
