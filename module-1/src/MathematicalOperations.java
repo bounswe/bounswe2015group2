50196ad2b7a766f42f17bf654bf502980007006d
onur <Onur NEVAT>
2015-05-03 15:58:20 +0300

    Division added

--- module-1/src/MathematicalOperations.java
+++ module-1/src/MathematicalOperations.java
@@ -1,29 +1,45 @@
-/**
- * Created by onureken on 02/05/15.
- */
 import java.lang.System;
 import java.util.Scanner;
+
 public class MathematicalOperations {
 
-    public static int dummyFunction(){
-         return 1;
-     } // Added by Onur Eken
-    public static int additionFunction(int a, int b){ return a+b; } // Added by Sýla Güler
+    public static int dummyFunction() {
+        return 1;
+    } // Added by Onur Eken
 
-    public static void main(String[] args){
+    public static int additionFunction(int a, int b) {
+        return a + b;
+    } // Added by Sï¿½la Gï¿½ler
+
+
+    /**
+     * Performs a/b and returns the result.
+     * @param a dividend
+     * @param b divisor
+     * @return quotient
+     */
+    public static double divisionFunction(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argument 'divisor' is 0");
+        } else {
+            return a / b;
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner input1 = new Scanner(System.in);
         System.out.println("Which operation you would like to try: \n" +
                 "For Dummy Function, Press 1:" +
                 "For Addition Function, Press2:");
         int functionChoice = input.nextInt();
-        if(functionChoice==1)
+        if (functionChoice == 1)
             dummyFunction();
-        else if (functionChoice==2) {
+        else if (functionChoice == 2) {
             Scanner input2 = new Scanner(System.in);
             System.out.println("Enter two numbers to add: ");
             int a = input2.nextInt();
             int b = input2.nextInt();
-            additionFunction(a,b);
+            additionFunction(a, b);
         }
     }
 }
