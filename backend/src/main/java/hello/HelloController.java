d02f240566be0cc511e9d56f32f3382699531c84
Mustafa Taha Kocyigit <mustafataha93@gmail.com>
2015-10-12 01:46:39 +0300

    Create project folders with compiling helloworld applications

---
+++ backend/src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by Mustafa Taha on 12.10.2015.
+ */
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
