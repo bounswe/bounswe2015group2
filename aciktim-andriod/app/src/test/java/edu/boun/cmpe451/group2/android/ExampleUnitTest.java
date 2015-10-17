97480281aa49bc73b9cadb362a9430a7764accc4
Mustafa Taha Kocyigit <mustafataha93@gmail.com>
2015-10-18 01:11:30 +0300

    Android Projecy Added

---
+++ aciktim-andriod/app/src/test/java/edu/boun/cmpe451/group2/android/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package edu.boun.cmpe451.group2.android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
