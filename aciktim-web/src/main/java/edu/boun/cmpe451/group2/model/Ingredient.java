de2d218101648138068b28f68aa8249e3fb13651
Murat Cenk Batman <muratcenkbatman@windowslive.com>
2015-11-02 21:43:27 +0200

    Ingredient model is completed

--- aciktim-web/src/main/java/edu/boun/cmpe451/group2/model/Ingredient.java
+++ aciktim-web/src/main/java/edu/boun/cmpe451/group2/model/Ingredient.java
@@ -4,5 +4,11 @@ package edu.boun.cmpe451.group2.model;
 public class Ingredient {
 
     public Long id = null;
-
+    public String name = "";
+    public double protein = 0;
+    public double fat = 0;
+    public double carbohydrate = 0;
+    public double calories = 0;
+    public Long unitID = null;
+    public String unitName= "";
 }
