3a076e77f9a93e89b7ebfad7817ac2c7add5ae53
Ugur Tombul <ugur2889@gmail.com>
2015-11-23 16:34:15 +0200

    Create semantic taging and todays consumptions activities

---
+++ aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/SemanticTagActivity.java
@@ -0,0 +1,19 @@
+package edu.boun.cmpe451.group2.android;
+
+import android.app.LoaderManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ugur_tombul on 22.11.2015.
+ */
+public class SemanticTagActivity extends AppCompatActivity
+
+{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semantic_tag);
+    }
+}
