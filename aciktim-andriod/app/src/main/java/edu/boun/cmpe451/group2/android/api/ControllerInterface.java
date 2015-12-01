120c4c746747ccd651e61c22dc7ba5f0ab694314
Mustafa Taha Kocyigit <mustafataha93@gmail.com>
2015-12-01 09:29:08 +0200

    Update api package inside android project with new api methods

--- aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/api/ControllerInterface.java
+++ aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/api/ControllerInterface.java
@@ -27,29 +27,28 @@ public interface ControllerInterface {
 
     public static final String EMAIL_PARAMETER = "email";
 
-    public static final String LOGIN_PATH = "/login";
+    public static final String LOGIN_PATH = "login";
 
-    public static final String USER_SVC_PATH = "/user";
+    public static final String USER_SVC_PATH = "user";
 
     public static final String RECIPE_SVC_PATH = "recipe";
 
-    public static final String RECIPE_TITLE_SEARCH_PATH = RECIPE_SVC_PATH + "/search/findByName";
+    public static final String RECIPE_TITLE_SEARCH_PATH = RECIPE_SVC_PATH + "search";
 
     public static final String RECIPE_LIST_BY_USER_PATH = RECIPE_SVC_PATH + "/list";
-    @FormUrlEncoded
     @POST(LOGIN_PATH)
-    String login(@Field(EMAIL_PARAMETER) String email, @Field(PASSWORD_PARAMETER) String password);
+    Call<Response> login(@Query(EMAIL_PARAMETER) String email, @Query(PASSWORD_PARAMETER) String password);
 
     @GET(USER_SVC_PATH)
-    User getUser(@Query(API_KEY_PARAMETER) String api_key);
+    Call<User> getUser(@Query(API_KEY_PARAMETER) String api_key);
 
     @POST(USER_SVC_PATH)
-    String signup(@Body User user);
+    Call<Response> signup(@Body User user);
 
     @POST(RECIPE_SVC_PATH)
-    String addrecipe(@Body Recipe recipe);
+    Call<Response> addrecipe(@Body Recipe recipe);
 
-    String deleteRecipe(@Query(RECIPE_ID_PARAMETER) Long recipeID);
+    Call<Response> deleteRecipe(@Query(RECIPE_ID_PARAMETER) Long recipeID);
 
     @GET(RECIPE_LIST_BY_USER_PATH)
     Call<List<Recipe>> getRecipes(@Query(USER_ID_PARAMETER) Long users_id);
@@ -58,21 +57,21 @@ public interface ControllerInterface {
     Call<Recipe> getRecipe(@Query(RECIPE_ID_PARAMETER) Long recipe_id) throws Exception;
 
     @PUT(RECIPE_SVC_PATH + "/update")
-    String updateRecipe(@Body Recipe recipe);
+    Call<Response> updateRecipe(@Body Recipe recipe);
 
     @GET(USER_SVC_PATH + "/recommendations")
-    List<Map<String, Object>> getRecommendations(@Body User user);
+    Call<List<Map<String, Object>>> getRecommendations(@Body User user);
 
-    @GET("/search")
-    ArrayList<Recipe> search(@Query(RECIPE_ID_PARAMETER) String name);
+    @GET(RECIPE_TITLE_SEARCH_PATH)
+    Call<ArrayList<Recipe>> search(@Query(RECIPE_ID_PARAMETER) String name);
 
-    @GET("/advancedSearch")
-    ArrayList<Recipe> search(@Query(RECIPE_ID_PARAMETER) String name,@Query(RECIPE_ID_PARAMETER) ArrayList<String> ingrNames);
+    @GET("advancedSearch")
+    Call<ArrayList<Recipe>> search(@Query(RECIPE_ID_PARAMETER) String name,@Query(RECIPE_ID_PARAMETER) ArrayList<String> ingrNames);
 
     @POST(USER_SVC_PATH+"/addMenu")
-    String addMenu(@Query(RECIPE_ID_PARAMETER) Menu menu);
+    Call<Response> addMenu(@Query(RECIPE_ID_PARAMETER) Menu menu);
 
     @GET(USER_SVC_PATH+"/getMenus")
-    HashMap<Long,Menu> getMenusByApiKey(@Query(RECIPE_ID_PARAMETER) String api_key);
+    Call<HashMap<Long,Menu>> getMenusByApiKey(@Query(RECIPE_ID_PARAMETER) String api_key);
 
 }
--- aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/api/Menu.java
+++ aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/api/Menu.java
@@ -1,6 +1,7 @@
 package edu.boun.cmpe451.group2.android.api;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Menu model class
@@ -11,5 +12,30 @@ public class Menu {
 
     public ArrayList<Recipe> recipes;
     public Long ownerID;
-    public String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(Long ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public ArrayList<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(ArrayList<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public String name = "";
 }
