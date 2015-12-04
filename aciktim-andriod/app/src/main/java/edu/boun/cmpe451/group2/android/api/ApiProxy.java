244668d8c1a1317b8e15fdaf47f4e267880c0b29
ugur tombul <ugur2889@gmail.com>
2015-12-04 15:55:18 +0200

    Some error are fixing

--- aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/api/ApiProxy.java
+++ aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/api/ApiProxy.java
@@ -5,14 +5,12 @@ import retrofit.Retrofit;
 /**
  * Created by Mustafa Taha on 1.12.2015.
  */
-public class ApiProxy {
-    ​
+
+public class ApiProxy  {
+
     public ControllerInterface getApi() {
-        return api;
-    }
-    ​
+        return api; }
     private ControllerInterface api;
-    ​
     public ApiProxy() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://ec2-52-28-126-194.eu-central-1.compute.amazonaws.com:8080/aciktim/api/")
