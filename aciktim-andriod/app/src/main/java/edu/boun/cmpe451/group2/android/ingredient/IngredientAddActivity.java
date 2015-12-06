e7b7293b0f36e0654b7d30b3a2cfa3131dc74498
Cafer Tayyar YORUK <cafertayyaryoruk@hotmail.com>
2015-12-06 17:27:04 +0200

    Ingredient-add page was added. Also, java classes were created in order to get ingredients from National Nutrient Database

---
+++ aciktim-andriod/app/src/main/java/edu/boun/cmpe451/group2/android/ingredient/IngredientAddActivity.java
@@ -0,0 +1,19 @@
+package edu.boun.cmpe451.group2.android.ingredient;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.boun.cmpe451.group2.android.R;
+
+/**
+ * Created by Cafer Tayyar YORUK on 06.12.2015.
+ */
+public class IngredientAddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ingredient_add);
+
+    }
+}
